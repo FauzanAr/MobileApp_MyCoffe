a93f0df165acaf6e6b26ce871ef175b18be23d37
FauzanAr <faurinta12345@gmail.com>
2019-10-10 16:06:51 +0800

    Adding function to progress bar also changing color at progress bar

--- app/src/main/java/android/jplas/mycoffee/activity/SplashScreen.java
+++ app/src/main/java/android/jplas/mycoffee/activity/SplashScreen.java
@@ -6,20 +6,41 @@ import android.content.Intent;
 import android.jplas.mycoffee.R;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        new Handler().postDelayed(new Runnable(){
+        new Thread(new Runnable() {
             @Override
-            public void run(){
-                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            public void run() {
+                doWork();
+                startApp();
                 finish();
             }
-        }, 3000);
+        }).start();
+    }
+
+    private void doWork(){
+        for (int progress = 0; progress<100; progress+=20){
+            try{
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (Exception e){
+                e.printStackTrace();
+                e.getMessage();
+            }
+        }
+    }
+
+    private void startApp(){
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
     }
 }
