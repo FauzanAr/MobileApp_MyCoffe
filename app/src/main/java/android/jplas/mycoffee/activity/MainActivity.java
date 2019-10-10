fe0534e8d0621a59f2185570b6e6368de8c35516
FauzanAr <faurinta12345@gmail.com>
2019-10-10 14:16:26 +0800

    Initial commit

---
+++ app/src/main/java/android/jplas/mycoffee/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package android.jplas.mycoffee.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.jplas.mycoffee.R;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
