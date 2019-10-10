b3e42272a0c377f371f5bc5448cf7534d718ac97
FauzanAr <faurinta12345@gmail.com>
2019-10-10 15:02:33 +0800

    Renaming img file and creating splash method at their activity

--- app/src/main/java/android/jplas/mycoffee/activity/SplashScreen.java
+++ app/src/main/java/android/jplas/mycoffee/activity/SplashScreen.java
@@ -2,8 +2,10 @@ package android.jplas.mycoffee.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.jplas.mycoffee.R;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
