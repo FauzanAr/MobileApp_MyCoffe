4549ea3e92566b34f77b86c426835ef02608d1f5
FauzanAr <faurinta12345@gmail.com>
2019-10-11 13:52:07 +0800

    Creating list view row then creating class to add it dynamically

---
+++ app/src/main/java/android/jplas/mycoffee/bankofclass/CustomListAdapter.java
@@ -0,0 +1,13 @@
+package android.jplas.mycoffee.bankofclass;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+
+public class CustomListAdapter extends ArrayAdapter {
+
+    private final Activity context;
+    private final Integer[] imageIDarray;
+    private final String[] nameArray;
+    private final String[] infoArray;
+
+}
