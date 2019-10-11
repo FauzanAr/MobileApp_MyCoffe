00251e436bf6f1c21e0cb206f4198bfdb6d446bc
FauzanAr <faurinta12345@gmail.com>
2019-10-11 14:02:31 +0800

    Adding array data to the fragment

--- app/src/main/java/android/jplas/mycoffee/fragment/coffeeFragment.java
+++ app/src/main/java/android/jplas/mycoffee/fragment/coffeeFragment.java
@@ -16,6 +16,17 @@ import android.jplas.mycoffee.R;
  */
 public class coffeeFragment extends Fragment {
 
+    String[] nameArray = {"Cappucino","Espresso","Mocaccino"};
+    String[] infoArray = {
+            "Is an espresso-based coffee drink that originated in Italy, and is traditionally prepared with steamed milk foam (microfoam).",
+            "Is coffee of Italian origin, brewed by forcing a small amount of nearly boiling water under pressure (expressing) through finely ground coffee beans.",
+            "Is a chocolate-flavored variant of a caffè latte. Other commonly used spellings are mochaccino and also mochachino."
+    };
+    Integer[] imageArray = {
+            R.drawable.cappucino,
+            R.drawable.espresso,
+            R.drawable.mocaccino
+    };
 
     public coffeeFragment() {
         // Required empty public constructor
