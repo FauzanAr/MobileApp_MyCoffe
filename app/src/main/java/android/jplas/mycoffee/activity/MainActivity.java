b17d659df20b6b34429f7fc940b9a2d4ce0a5993
FauzanAr <faurinta12345@gmail.com>
2019-10-11 10:46:25 +0800

    Creating function to handle menu movement

--- app/src/main/java/android/jplas/mycoffee/activity/MainActivity.java
+++ app/src/main/java/android/jplas/mycoffee/activity/MainActivity.java
@@ -1,15 +1,61 @@
 package android.jplas.mycoffee.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.fragment.app.Fragment;
 
 import android.jplas.mycoffee.R;
+import android.jplas.mycoffee.fragment.coffeeFragment;
+import android.jplas.mycoffee.fragment.homeFragment;
+import android.jplas.mycoffee.fragment.shopFragment;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new homeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_home:
+                fragment = new homeFragment();
+                break;
+            case R.id.action_shop:
+                fragment = new shopFragment();
+                break;
+            case R.id.action_coffee:
+                fragment = new coffeeFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
