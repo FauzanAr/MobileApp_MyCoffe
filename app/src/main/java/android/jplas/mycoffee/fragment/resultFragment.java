25562d5817363656fb85838d5f5749f82d640336
FauzanAr <faurinta12345@gmail.com>
2019-10-14 00:48:56 +0800

    Adding new fragment

---
+++ app/src/main/java/android/jplas/mycoffee/fragment/resultFragment.java
@@ -0,0 +1,32 @@
+package android.jplas.mycoffee.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.jplas.mycoffee.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class resultFragment extends Fragment {
+
+
+    public resultFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+
+}
