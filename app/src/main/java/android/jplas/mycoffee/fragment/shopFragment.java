b0572d63511964993251490e8994c00df2274629
FauzanAr <faurinta12345@gmail.com>
2019-10-14 00:54:25 +0800

    Creating method oncreat at main fragment

--- app/src/main/java/android/jplas/mycoffee/fragment/shopFragment.java
+++ app/src/main/java/android/jplas/mycoffee/fragment/shopFragment.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.jplas.mycoffee.R;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class shopFragment extends Fragment {
+public class shopFragment extends Fragment implements View.OnClickListener {
 
+    View view;
+    Button button;
+    RadioGroup radioGroup;
+    EditText textNama, textJumlah;
 
     public shopFragment() {
         // Required empty public constructor
@@ -26,6 +34,19 @@ public class shopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        view = inflater.inflate(R.layout.fragment_shop, container, false);
+
+        textNama = view.findViewById(R.id.text_nama);
+        textJumlah = view.findViewById(R.id.text_jumlah);
+        radioGroup = view.findViewById(R.id.radio_group);
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
