1279cc59198b6fe55ff2af28ec07baef1d035bb6
FauzanAr <faurinta12345@gmail.com>
2019-10-14 00:24:13 +0800

    Creating parcelable class for shop interface

---
+++ app/src/main/java/android/jplas/mycoffee/bankofclass/Coffee.java
@@ -0,0 +1,68 @@
+package android.jplas.mycoffee.bankofclass;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Coffee implements Parcelable {
+    public final int CAPPUCINO = 0;
+    public final int ESPRESSO = 1;
+    public final int MOCACCINO = 2;
+
+    private String nama;
+    private int jumlah, total, tipe;
+
+    public Coffee(String nama, int jumlah, int tipe) {
+        this.nama = nama;
+        this.jumlah = jumlah;
+        this.total = calculate();
+        this.tipe = tipe;
+    }
+
+    private int calculate(){
+        switch (tipe){
+            case CAPPUCINO: return jumlah * 25000;
+            case ESPRESSO: return jumlah * 30000;
+            case MOCACCINO: return jumlah * 15000;
+            default: return 0;
+        }
+    }
+    public String getNama() {
+        return nama;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    protected Coffee(Parcel in) {
+        nama = in.readString();
+        jumlah = in.readInt();
+        total = in.readInt();
+        tipe = in.readInt();
+    }
+
+    public static final Creator<Coffee> CREATOR = new Creator<Coffee>() {
+        @Override
+        public Coffee createFromParcel(Parcel in) {
+            return new Coffee(in);
+        }
+
+        @Override
+        public Coffee[] newArray(int size) {
+            return new Coffee[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(nama);
+        parcel.writeInt(jumlah);
+        parcel.writeInt(total);
+        parcel.writeInt(tipe);
+    }
+}
--- app/src/main/java/android/jplas/mycoffee/fragment/shopFragment.java
+++ app/src/main/java/android/jplas/mycoffee/fragment/shopFragment.java
@@ -28,5 +28,4 @@ public class shopFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shop, container, false);
     }
-
 }
